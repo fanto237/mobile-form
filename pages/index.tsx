a3780959bdc961c5a752fce16f88efbcd1e378ea
Lucien <58596314+fantoSama@users.noreply.github.com>
2022-07-19 16:27:04 +0200

    updating index to upload file

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,32 +2,48 @@ import type { NextPage } from "next";
 
 const Home: NextPage = () => {
   return (
-    <div className="flex justify-center items-center h-screen bg-slate-200">
-      <div className="block bg-slate-50 p-6 rounded-xl shadow-md  shadow-slate-300 w-[90]">
+    <div className="flex items-center justify-center h-screen bg-slate-200">
+      <div className="block w-full p-6 shadow-md bg-slate-50 rounded-xl shadow-slate-300">
         <form action="">
-          <h2 className="text-[#ff6a39] text-justify text-3xl font-semibold my-4">
-            Schaden Formular
+          <h2 className="text-[#ff7b27]  text-3xl text-center font-semi bold my-4">
+            Digitalisierter Aufbauzettel
           </h2>
 
-          <label className="text-sm">Unternehmen</label>
+          <label className="text-xl">Unternehmen: </label>
           <input
-            type="emial"
+            type="text"
             name=""
             id="email"
-            className="h-8 w-full rounded-md border border-slate-300 text-sm pl-2 bg-transparent outline-blue-600 shadow-sm"
+            className="w-full h-8 pt-2 mb-5 text-xl bg-transparent border rounded-md shadow-sm border-slate-300 "
           />
-          <label className="text-sm">Email</label>
+          <label className="text-xl">Ort: </label>
+          <button className="w-full h-8 pt-2  mb-5 text-xl bg-transparent border rounded-md shadow-sm border-slate-300 " />
+          <label className="text-xl">Datum: </label>
           <input
-            type="emial"
+            type="date"
             name=""
             id="email"
-            className="h-8 w-full rounded-md border border-slate-300 text-sm pl-2 bg-transparent outline-blue-600 shadow-sm"
+            className="w-full h-8 pt-2 mb-5 text-xl bg-transparent border rounded-md shadow-sm border-slate-300 "
+          />
+          <label className="text-xl">Kennzeichnen:</label>
+          <input
+            type="file"
+            name=""
+            id="email"
+            className="w-full h-8  mb-5 text-xl bg-transparent border rounded-md shadow-sm border-slate-300 "
+          />
+          <label className="text-xl">Dachwerbeschild:</label>
+          <input
+            type="number"
+            name=""
+            id="email"
+            className="w-full h-8 pt-2 mb-5 text-xl bg-transparent border rounded-md shadow-sm border-slate-300 "
           />
           <input
             type="submit"
             name=""
             id="signUp"
-            className="bg-[#ff7b27] w-full h-10 cursor-pointer text-white rounded-md nav-item text-sm"
+            className="bg-[#ff7b27] w-full h-10  mt-4 cursor-pointer text-white hover:scale-100 rounded-md nav-item text-xl"
           />
         </form>
       </div>
